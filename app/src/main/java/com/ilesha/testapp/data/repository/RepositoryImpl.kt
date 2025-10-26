2d582fe745c34da405ebfcfe8eb4159a434ab20d
ilesha23 <ilesha0123@gmail.com>
2025-10-26 20:07:52 +0200

    added repository implementation

---
+++ app/src/main/java/com/ilesha/testapp/data/repository/RepositoryImpl.kt
@@ -0,0 +1,41 @@
+package com.ilesha.testapp.data.repository
+
+import com.ilesha.testapp.data.api.ApiInterface
+import com.ilesha.testapp.data.mapper.toDomainModel
+import com.ilesha.testapp.domain.model.Entity
+import com.ilesha.testapp.domain.model.Result
+import com.ilesha.testapp.domain.repository.Repository
+import okio.IOException
+
+class RepositoryImpl(
+    private val api: ApiInterface
+) : Repository {
+
+    override suspend fun getAllIds(): Result<List<Int>> {
+        return try {
+            val idDto = api.getAllIds()
+            val idList = idDto.data?.let { dtoList ->
+                dtoList.mapNotNull { dtoItem ->
+                    dtoItem.id
+                }
+            } ?: emptyList()
+            Result.Success(idList)
+        } catch (e: IOException) {
+            Result.Error(e)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    override suspend fun getItemById(id: Int): Result<Entity> {
+        return try {
+            val item = api.getItemById(id).toDomainModel()
+            Result.Success(item)
+        } catch (e: IOException) {
+            Result.Error(e)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+}
