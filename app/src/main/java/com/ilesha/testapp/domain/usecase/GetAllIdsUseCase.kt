a6b4b8c17442457741226db69faf048bc2314971
ilesha23 <ilesha0123@gmail.com>
2025-10-26 20:35:38 +0200

    fixed use case return type

--- app/src/main/java/com/ilesha/testapp/domain/usecase/GetAllIdsUseCase.kt
+++ app/src/main/java/com/ilesha/testapp/domain/usecase/GetAllIdsUseCase.kt
@@ -1,6 +1,5 @@
 package com.ilesha.testapp.domain.usecase
 
-import com.ilesha.testapp.domain.model.Entity
 import com.ilesha.testapp.domain.model.Result
 import com.ilesha.testapp.domain.repository.Repository
 
@@ -8,7 +7,7 @@ class GetAllIdsUseCase(
     private val repository: Repository
 ) {
 
-    suspend operator fun invoke(): Result<List<Entity>> {
+    suspend operator fun invoke(): Result<List<Int>> {
         return repository.getAllIds()
     }
 
