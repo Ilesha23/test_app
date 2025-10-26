d6201a5bb28b664f4042bf35ee131580a855b66b
ilesha23 <ilesha0123@gmail.com>
2025-10-26 21:28:30 +0200

    added hilt navigation compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     // Hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     // Tests, debug, etc.
     testImplementation(libs.junit)
