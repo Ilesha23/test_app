9dea274a0df082d3220fe7ccf1ba0b59059a7baa
ilesha23 <ilesha0123@gmail.com>
2025-10-26 20:06:36 +0200

    fixed Result type

--- app/src/main/java/com/ilesha/testapp/domain/model/Result.kt
+++ app/src/main/java/com/ilesha/testapp/domain/model/Result.kt
@@ -1,6 +1,6 @@
 package com.ilesha.testapp.domain.model
 
-sealed class Result<T> {
+sealed class Result<out T> {
 
     data class Success<T>(val data: T) : Result<T>()
     data class Error(val exception: Throwable) : Result<Nothing>()
